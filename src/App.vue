ee35df04539cf5dbe77f4d5551cd99c3d589e03d
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-10-25 21:56:46 -0500

    Cotizador de criptomonedas

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Cotizador de Criptomonedas</h1>
+
+    <Grid>
+      <formulario />
+      <Data />
+    </Grid>
+  </div>
+</template>
+
+<script>
+import Formulario from "./components/Formulario.vue";
+import Data from "./components/Data.vue";
+import Grid from "./components/Grid.vue";
+export default {
+  components: {
+    Formulario,
+    Data,
+    Grid,
+  },
+};
+</script>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <form>
+      <div>
+        <label>Moneda Nacional:</label>
+        <select required>
+          <option value="" disabled selected>Selecciona la Moneda ....</option>
+          <option>MXN</option>
+          <option>USD</option>
+          <option>EUR</option>
+        </select>
+      </div>
+      <div>
+        <label>Criptomoneda:</label>
+        <select required>
+          <option value="" disabled selected>
+            Selecciona la Criptomoneda ....
+          </option>
+          <option>BTC</option>
+          <option>ETH</option>
+          <option>LTC</option>
+        </select>
+      </div>
+      <div>
+        <button type="submit">Cotizar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
